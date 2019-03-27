0b7070c66bda7b61792161b7a1b0f421383a1e4f
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-27 18:51:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,5 +6,8 @@ def key_for_min_value(name_hash)
   min_value_key = name_hash[0]
   name_hash.each do |a,b|
     if b < min_value
-      
+      min_value_key = b 
+    end
+  end
+  min_value_key
 end
